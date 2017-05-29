2beba8757ba21f776b28ae358d4d33ba29fa5ff7
Meghana Singh <meghana1602@gmail.com>
2017-05-29 05:37:38 +0000

    Added the app welcome index page and about page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   def avatar_url(size)
      gravatar_id = Digest::MD5::hexdigest(self.email).downcase
      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-   end    
+  end    
 end
