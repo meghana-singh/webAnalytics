be9aa4aec06b6311673b54e92903326dde1d9f24
Meghana Singh <meghana1602@gmail.com>
2017-06-05 19:25:17 +0000

    Get error while trying to add event through API call

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+     resources :events, only: [:create]
+  end
+  
   root 'registered_applications#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
