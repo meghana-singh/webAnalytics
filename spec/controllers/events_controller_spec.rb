13b2e3a1da2243b6e5305c6507db811e88701c03
Meghana Singh <meghana1602@gmail.com>
2017-06-06 14:52:31 +0000

    Rspec tests passing

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,25 +2,26 @@ require 'rails_helper'
 
 
 RSpec.describe API::EventsController, type: :controller do
-    
-  describe 'not signed in' do
-      
-    let(:user)                           { create(:user) }
-    let(:registered_application)         { create(:registered_application, user: user) }
-    
+  let(:user)                           { create(:user) }
+  let(:registered_application)         { create(:registered_application, user: user) }
+  
+  describe "POST create" do
     before do
-      @user   = user
-      sign_in   @user
+      controller.request.env['HTTP_ORIGIN'] = registered_application.url
+      post :create, params: {event: { name: "About Page Load" }}
     end
-
-    describe 'POST create', :type => :api do
-      
-      it 'assigns the new event to @event' do
-         URI "https://lit-bayou-47361.herokuapp.com/users/sign_in",:format =>:json
-         post :create, params: {id: registered_application.id, event: {name: Faker::Company.name}}
-         expect(assigns(:event)).to eq Event.last
-      end
-
+    
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "returns json content type" do
+      expect{JSON.parse(response.body)}.not_to raise_exception
+    end
+    
+    it "creates a event with the correct attributes" do
+      json = JSON.parse(response.body)
+      expect(json["name"]).to eq("About Page Load")
     end
   end
 end
